9ccdc45cf164fb630edfa35086ece1e16006171d
Zahary Karadjov <zahary@metacraft-labs.com>
2026-04-22 15:56:17 +0300

    style: run cargo fmt on local sources

--- src/replay.rs
+++ src/replay.rs
@@ -198,7 +198,9 @@ pub fn replay_transaction(
 
     let events_filename = match format {
         TraceEventsFileFormat::Json => "trace.json",
-        TraceEventsFileFormat::Binary | TraceEventsFileFormat::BinaryV0 | TraceEventsFileFormat::Ctfs => "trace.bin",
+        TraceEventsFileFormat::Binary
+        | TraceEventsFileFormat::BinaryV0
+        | TraceEventsFileFormat::Ctfs => "trace.bin",
     };
     let events_path = out_dir.join(events_filename);
     let metadata_path = out_dir.join("trace_metadata.json");
